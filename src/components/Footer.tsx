27e9133b78ca2d14f06fb0a4a59bd9e9e80f3dd3
pgobin <pgobin@uw.edu>
2025-06-27 22:57:12 -0700

    removed links to TOS and Privacy pages (could add them back in the future if need be)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,10 @@
 import React from "react";
-import Link from "next/link";
 
 function Footer() {
     return (
-        <>
-            <footer className="flex flex-col text-black h-16 flex justify-center items-center">
-                <p className="text-center">&copy; 2025 UWB ACM</p>
-                <div className="flex flex-row gap-5">
-                    <Link href="/tos">Terms of Service</Link>
-                    <Link href="/privacy">Privacy Policy</Link>
-                </div>
-            </footer>
-        </>
+        <footer className="flex-col text-black h-16 flex justify-center items-center">
+            <p className="text-center">&copy; 2025 UWB ACM</p>
+        </footer>
     );
 }
 
